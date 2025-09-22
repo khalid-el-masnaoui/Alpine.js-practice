41e5dbb63ec4875da10fe50cabd0779e391f9b98
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-09-22 22:11:41 +0100

    setup components code

---
+++ components/tailwind.config.js
@@ -0,0 +1,8 @@
+    // tailwind.config.js
+    module.exports = {
+      content: [], // You can leave this empty or point it to your HTML files if desired for future local builds
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };
