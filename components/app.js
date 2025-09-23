eff993906b247cfc39c3ac6ffa57be8804dbccc9
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-09-23 07:07:24 +0100

    adding the title and body for the notification messsage with the notification tyoe icon

--- components/app.js
+++ components/app.js
@@ -31,11 +31,11 @@ document.addEventListener("alpine:init", () => {
         notifications: [],
         visible: [],
         add(notification) {
-            if (notification.message.body == null || notification.message.body == "") {
-                return;
-            }
+            // if (notification.message.body == null || notification.message.body == "") {
+            //     return;
+            // }
             notification.id = Date.now();
-            notification.duration = notification.duration ?? 1000;
+            notification.duration = notification.duration ?? 800000000;
             this.notifications.push(notification);
             this.fire(notification);
         },
@@ -58,10 +58,33 @@ document.addEventListener("alpine:init", () => {
 		getThemeColor(type) {
 			switch (type.toLowerCase()) {
 				case 'success': return 'green'
-				case 'warning': return 'orange'
+				case 'warning': return 'yellow'
                 case 'error': return 'red'
 				default: return 'blue'
 			}
-		},
+        },
+        getNotificationTypeIcon(type) {
+            switch (type.toLowerCase()) {
+				case 'success': return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>`
+
+				case 'warning': return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                </svg>`
+
+                case 'error': return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                </svg>`
+                
+				default: return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg>`
+            }
+        },
+
+        // init() {
+        //     this.add({ type: "success", message: { title: "success", body: "hello there ÞTenetur et illum iste et est. Reprehenderit corporis impedit maiores. Ducimus qui maxime ut impedit iure ea. Qui rerum sed eius repudiandae est possimus. Similique porro repudiandae." }})
+        // }
     }))
 })
