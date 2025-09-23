8d200a437be3e9f37394605d0cdcffd814205ea3
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-09-23 04:09:47 +0100

    adding main accordion functionality

--- components/app.js
+++ components/app.js
@@ -9,4 +9,13 @@ document.addEventListener("alpine:init", () => {
         activeTab: 'Home'
     }))
 
+     Alpine.data("accordionComponent", () => ({
+        accordions: {
+            'topic #1': 'Commodi vel tempora. Ex sit delectus nihil voluptas ratione provident dolor optio iusto. Vel omnis quia qui aut assumenda. Cum cumque veritatis repellendus nam maxime minima a dolores.',
+            'topic #2': 'Nemo est et et harum iste natus nesciunt neque.',
+            'topic #3': 'Vitae aut hic est atque qui commodi voluptatum. Dolore nesciunt consectetur dolorem nesciunt. Ducimus quis quam et neque in.'
+        },
+        activeAccordion: null
+    }))
+
 })
