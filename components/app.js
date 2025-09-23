a630d4aa9115dce7daeb850afd3f9636170aa73e
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-09-23 06:14:51 +0100

    adding main functionality of notification component with the notification-handler componenet

--- components/app.js
+++ components/app.js
@@ -16,6 +16,58 @@ document.addEventListener("alpine:init", () => {
             'topic #3': 'Vitae aut hic est atque qui commodi voluptatum. Dolore nesciunt consectetur dolorem nesciunt. Ducimus quis quam et neque in.'
         },
         activeAccordion: null
+     }))
+    
+    
+    Alpine.data("notificationComponent", () => (    {
+        types : ['Success', 'Info', 'Warning', 'Error'],
+        openTypeDropdown : false,
+        selectedType : 'Success',
+        message: null
     }))
 
+
+    Alpine.data("notificationHandler", () => (    {
+        notifications: [],
+        visible: [],
+        add(notification) {
+            if (notification.message == null || notification.message == "") {
+                return;
+            }
+            notification.id = Date.now();
+            notification.animationTime = this.getAnimationTime()
+            this.notifications.push(notification);
+            console.log(this.notifications)
+            this.fire(notification);
+        },
+        //FIFO
+        fire(notification) {
+            this.visible.push(notification);
+            setTimeout(() => {
+                this.remove(notification.id);
+            }, notification.animationTime);
+        },
+        remove(id) {
+            const notification = this.visible.find((notification) => notification.id == id);
+            const index = this.visible.indexOf(notification);
+            this.visible.splice(index, 1);
+            this.notifications.splice(index, 1);
+        },
+        getAnimationTime() {
+			const delta = 1
+			const transitionTime = 800
+			return (7000 * delta) - (transitionTime * delta)
+		},
+		getBgColor(type, shade) {
+			return `bg-${this.getThemeColor(type)}-${shade}`
+		},
+		getThemeColor(type) {
+			switch (type.toLowerCase()) {
+				case 'success': return 'green'
+				case 'warning': return 'orange'
+                case 'error': return 'red'
+				default: return 'blue'
+			}
+		},
+    }))
 })
